aabf50aaf581c63fc477455e74120f2b40156c6d
oleg <o.panchenko@gmail.com>
2014-08-14 10:03:28 +0300

    add avatar for user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def avatar_url(user, size=200)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      default_url = "#{root_url}images/guest.png"
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    end
+  end
 end
